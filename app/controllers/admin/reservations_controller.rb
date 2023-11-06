d6ef7b16700070da8841b4e51b6ce3001ac36e33
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-11-06 13:25:03 +0900

    [admin/reservations.controller.rbのリファクタリング完了]

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show update destroy]
+  before_action :load_movies_and_schedules_and_sheets, only: %i[new create]
+
   def index
     @reservations = Reservation.all
   end
@@ -16,45 +19,25 @@ class Admin::ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
-
-    begin
-      if @reservation.save
-        flash[:notice] = '予約の作成に成功しました'
-        redirect_to admin_reservations_path
-      else
-        Rails.logger.info(@reservation.errors.full_messages) # この行を修正しました
-        flash.now[:error] = @reservation.errors.full_messages.to_sentence
-        @movies = Movie.all
-        @schedules = Schedule.includes(:movie).all
-        @sheets = Sheet.all
-        render :new, status: :bad_request
-      end
-    rescue ActiveRecord::RecordNotUnique
-      flash.now[:error] = 'その座席はすでに予約済みです'
-      @reservation = Reservation.new
-      @movies = Movie.all
-      @schedules = Schedule.all
-      @sheets = Sheet.all
-      render :new
+    if save_reservation
+      redirect_to admin_reservations_path, notice: '予約の作成に成功しました'
+    else
+      render :new, status: :bad_request
     end
+  rescue ActiveRecord::RecordNotUnique
+    handle_reservation_conflict
   end
 
   def update
-    reservation = Reservation.find(params[:id])
-    begin
-      if reservation.update(reservation_params)
-        redirect_to admin_reservations_path
-        flash[:notice] = '予約の更新に成功しました'
-      else
-        flash.now[:error] = 'その座席はすでに予約済みです'
-        @reservation = Reservation.find(params[:id])
-        render :show
-      end
-    rescue ActiveRecord::RecordNotUnique
-      flash.now[:error] = 'その座席はすでに予約済みです'
-      @reservation = Reservation.find(params[:id])
+    if update_reservation
+      redirect_to admin_reservations_path, notice: '予約の更新に成功しました'
+    else
+      flash.now[:error] = '更新に失敗しました'
       render :show
     end
+  rescue ActiveRecord::RecordNotUnique
+    flash.now[:error] = 'その座席はすでに予約済みです'
+    render :show
   end
 
   def destroy
@@ -69,6 +52,46 @@ class Admin::ReservationsController < ApplicationController
   end
 
   private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # 共通のコードをロードするbefore_action
+  def load_movies_and_schedules_and_sheets
+    @movies = Movie.all
+    @schedules = Schedule.includes(:movie).all
+    @sheets = Sheet.all
+  end
+
+  # 予約の保存処理を抽出したメソッド
+  def save_reservation
+    if @reservation.save
+      true
+    else
+      Rails.logger.info(@reservation.errors.full_messages)
+      flash.now[:error] = @reservation.errors.full_messages.to_sentence
+      false
+    end
+  end
+
+  def update_reservation
+    @reservation.assign_attributes(reservation_params)
+    if @reservation.save
+      true
+    else
+      Rails.logger.info(@reservation.errors.full_messages)
+      flash.now[:error] = @reservation.errors.full_messages.to_sentence
+      false
+    end
+  end
+
+  # 座席衝突時のエラーハンドリングを抽出したメソッド
+  def handle_reservation_conflict
+    flash.now[:error] = 'その座席はすでに予約済みです'
+    @reservation = Reservation.new(reservation_params)
+    render :new
+  end
 
   def reservation_params
     params.require(:reservation).permit(:date, :name, :email, :schedule_id, :sheet_id)
