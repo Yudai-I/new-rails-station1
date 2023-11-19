0ceb1cab5cfec1c1cd28a6a98dd3fd41693fb0d1
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-11-19 17:51:16 +0900

    [映画予約時用のActionmailer導入]

--- db/migrate/20231022065247_create_reservations.rb
+++ db/migrate/20231022065247_create_reservations.rb
@@ -1,18 +1,5 @@
-# CreateReservationsクラスは、予約情報を管理するための`reservations`テーブルを作成します。
-# このテーブルには、映画の上映スケジュールに対する顧客の予約詳細が含まれます。
-#
-# == カラム詳細:
-# - date: date, 予約日を格納します。NULLを許可しません。
-# - schedule_id: integer, 予約された上映スケジュールのIDを参照します。NULLを許可しません。
-# - sheet_id: integer, 予約された座席のIDを参照します。NULLを許可しません。
-# - email: string, 予約者のメールアドレスを格納します。NULLを許可しません。
-# - name: string, 予約者の名前を格納します。NULLを許可しません。
-# - timestamps: レコードの作成日時と更新日時を自動記録するためのカラムです。
-#
-# == インデックス:
-# 予約日、スケジュールID、座席IDの組み合わせに一意性の制約を設けることで、
-# 同じスケジュール・同じ座席に対する重複予約を防ぎます。
-#
+# 予約情報を管理するためのテーブル
+
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
