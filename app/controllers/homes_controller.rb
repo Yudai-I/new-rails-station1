508b03f56b0e36b1f00104c85e05c6b67e4e60a3
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-12-01 01:38:52 +0900

    コードテストをするための捨てコードから、タスクを正常に実行するためのコードに変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,7 @@
 class HomesController < ApplicationController
   def top
     @today = Date.today.strftime('%Y-%m-%d')
-    last_thirty_day = Date.today - 30
-    # 今日の日付から30日前までの予約を全取得
-    @reservations = Reservation.joins(:schedule).where("schedules.schedule_date >= ?", last_thirty_day)
-    @movie_reservations = Reservation.joins(schedule: :movie)
-                                     .where("schedules.schedule_date >= ?", last_thirty_day)
-                                     .group('movies.id')
-                                     .count
+    last_thirty_date = Date.today - 30
+    @rankings = Ranking.where("date_ranked >= ?", last_thirty_date)
   end
 end
