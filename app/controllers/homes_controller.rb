0c6069f658124f83e60c822796832a2bdb96fe76
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-12-02 22:49:05 +0900

    topメソッドでデータベースの検索をしていたが、そもそも検索しなくてもよかったため修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @today = Date.today.strftime('%Y-%m-%d')
-    last_thirty_date = Date.today - 30
-    @rankings = Ranking.where("date_ranked >= ?", last_thirty_date)
+    @today = Date.today
+    @rankings = Ranking.where(date_ranked: @today)
   end
 end
