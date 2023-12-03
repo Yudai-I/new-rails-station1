9bc7c75675c571a6a90ba08d1d2c4a7df5e8b192
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-12-03 20:45:13 +0900

    任意の日付と比べて現在とのランキング比較をするために@rankings_for_selectを用意した。現在と任意の日付の順位のインデックスの差を比較してランキングの変動を調べるために@rankings_for_compareを用意した。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,10 @@ class HomesController < ApplicationController
   def top
     @today = Date.today
     @rankings = Ranking.where(date_ranked: @today)
+                       .order(count_reservations: :desc)
+    @rankings_for_select = Ranking.all
+    @rankings_for_compare = Ranking.where(date_ranked: params[:date_ranked])
+                              .order(count_reservations: :desc)
+                              .pluck(:movie_id)                
   end
 end
