5afe6f8935d4aa238ad91f4f37afd4245a6b9993
root <root@localhost>
2023-12-08 14:46:55 +0000

    重要ではない微調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,9 @@ class HomesController < ApplicationController
     @today = Date.today
     @rankings = Ranking.where(date_ranked: @today)
                        .order(count_reservations: :desc)
-    @rankings_for_select = Ranking.all
+    #　任意の過去の日付を選んで、現在とのランキング変動を調べるための選択肢
+    @rankings_for_select = Ranking.select(:date_ranked).distinct
+    # 選択した過去の日付からランキングのデータを抽出
     @rankings_for_compare = Ranking.where(date_ranked: params[:date_ranked])
                               .order(count_reservations: :desc)
                               .pluck(:movie_id)                
