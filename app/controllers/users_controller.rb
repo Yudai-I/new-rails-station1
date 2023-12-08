f71ca243f41dbb7e0c000dce0d48b4baf8460e50
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-12-08 18:20:26 +0900

    ユーザページにおける予約取り消し機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @reservations = Reservation.where(email: current_user.email)
   end
+
+  def destroy
+    reservation = Reservation.find(params[:id])
+    if reservation.destroy
+      flash[:notice] = "予約の取り消しに成功しました"
+      redirect_to user_path(current_user.id)
+    else
+      @reservations = Reservation.where(email: current_user.email)
+      render :show
+    end
+  end
+
 end
